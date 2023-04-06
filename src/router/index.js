7ba7d796524dc717e7d3ad3192d1e74484eaf93b
JackSmith10996 <20052540@stu.mmu.ac.uk>
2023-04-06 02:10:06 +0100

    Charts & User Pages

--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Register.vue')
+    },
+    {
+      path: '/HomePageChart',
+      name: '/HPChart',
+      component: () => import('../views/HomePageChart.vue')
+
+
+
     }
   ]
 })
