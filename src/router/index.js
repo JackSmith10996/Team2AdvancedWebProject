990923cb0387442e7ff3ef5c1d0e0e67a657a67e
Asa Bardsley <asatrublu05@gmail.com>
2023-04-25 16:33:04 +0100

    started add data page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/home',
+      name: 'Home',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/HomeView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -56,6 +64,14 @@ const router = createRouter({
       path: '/ForgottenPassword',
       name: '/ForgottenPass',
       component: () => import('../views/ForgottenPassword.vue')
+
+
+
+    },
+    {
+      path: '/AddData',
+      name: '/AddData',
+      component: () => import('../views/AddData.vue')
 
 
 
---
+++ src/views/AddData.vue
@@ -0,0 +1,133 @@
+<script setup>
+import { ref, watch } from "vue";
+import { useRouter } from "vue-router";
+import { firebaseAuthentication, query } from "@/firebase/database";
+import { progressProps } from "element-plus";
+
+let emit = defineEmits(["addData"])
+defineProps({
+    data: {
+        type: Array,
+        default: () => [],
+    },
+})
+
+// data variables
+const ledv = ref("");
+const lesv = ref("");
+const lsv = ref("");
+const lvef = ref("");
+const lvmass = ref("");
+const redv = ref("");
+const resv = ref("");
+const rsv = ref("");
+const rvef = ref("");
+
+const router = useRouter();
+
+function submitData() {
+    emit("addData", ledv.value, lesv.value, lsv.value, lsv.value, lvef.value, lvmass.value, redv.value, resv.value, rsv.value, rvef.value)
+
+    router.push("/Home");
+}
+
+</script>
+
+<template>
+    <el-form label-width="95px" class="demo-ruleForm" @submit.prevent>
+    <h2>Add Data</h2>
+
+        <el-form-item label="ledv">
+            <el-input
+            type="text"
+            placeholder="ledv"
+            required
+            autocomplete="off"
+            v-model="levd"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="lesv">
+            <el-input
+            type="text"
+            placeholder="lesv"
+            required
+            autocomplete="off"
+            v-model="lesv"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="lsv">
+            <el-input
+            type="text"
+            placeholder="lsv"
+            required
+            autocomplete="off"
+            v-model="lsv"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="lvef">
+            <el-input
+            type="text"
+            placeholder="lvef"
+            required
+            autocomplete="off"
+            v-model="lvef"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="lvmass">
+            <el-input
+            type="text"
+            placeholder="lvmass"
+            required
+            autocomplete="off"
+            v-model="lvmass"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="redv">
+            <el-input
+            type="text"
+            placeholder="redv"
+            required
+            autocomplete="off"
+            v-model="redv"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="resv">
+            <el-input
+            type="text"
+            placeholder="resv"
+            required
+            autocomplete="off"
+            v-model="resv"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="rsv">
+            <el-input
+            type="text"
+            placeholder="rsv"
+            required
+            autocomplete="off"
+            v-model="rsv"
+            ></el-input>
+        </el-form-item>
+        <el-form-item label="rvef">
+            <el-input
+            type="text"
+            placeholder="rvef"
+            required
+            autocomplete="off"
+            v-model="rvef"
+            ></el-input>
+        </el-form-item>
+
+        <el-form-item>
+      <el-button type="danger" @click="submitData">Submit</el-button>
+      <router-link :to="{ name: 'Home' }"
+        ><el-button>Cancel</el-button></router-link
+      >
+    </el-form-item>
+        
+    </el-form>
+
+</template>
+
+<style></style>
