6bc71b7b6de95eaf029934046a6be115025385d2
mohammad0161 <20042285@stu.mmu.ac.uk>
2023-05-01 15:10:36 +0100

    filling the home and help page with information.

--- src/views/HomePageChart.vue
+++ src/views/HomePageChart.vue
@@ -56,6 +56,28 @@ const chartOptions = ref({
 
 </script>
 <template>
+    <div id="cardiomyopathy-info">
+        <h3>
+            <i>Cardiomyopathy </i>is a term used to describe diseases that affect the heart muscle's
+                ability to pump blood to all body parts. Diseases are caused when the walls of the heart chambers
+                thicken, stretch or become stiff. The majority of cardiomyopathy types are genetic in nature and 
+                tend to manifest in children and younger individuals.<br><br>
+                Most common types of cardiomyopathy diseases include:
+            <ul><br>
+                <li><i>Dilated cardiomyopathy: </i>the heart walls become stretched and thin, reducing its ability to pump blood properly. if the heart is unable 
+                    to pump blood at an appropriate pressure, it could lead to heart failure.
+                </li><br>
+                <li><i>Hypertrophic cardiomyopathy: </i>The heart walls becomes thickened, this makes it harder for blood to flow out of the heart.
+                    eventhoght it may not have an impact on the individuals daily life, it is the leading cause of unexpected sudden death in childhood and among young athletes.
+                </li><br> 
+                <li><i>Arrhythmogenic right ventricular cardiomyopathy: </i>the heart muscle walls become thin, stretched and 
+                    cannot pump blood around the body effectively. The proteins responsible for holding the heart muscle
+                    cells together are abnormal, It is rare but it can cause abnormal heart rhythms and even sudden 
+                    cardiac death.
+                </li> 
+            </ul>
+        </h3>   
+    </div>
     <div>
         <apexchart
         postion = "center"
@@ -66,6 +88,7 @@ const chartOptions = ref({
         :series="series"
         ></apexchart>
     </div>
+    
     <div>
         <apexchart
         type="bar"
@@ -76,3 +99,18 @@ const chartOptions = ref({
         ></apexchart>
     </div>
 </template>
+<style>
+    #cardiomyopathy-info{
+        display: inline;
+        float: right;
+        font-size: ;
+        width: 52%;
+        margin-right: 3%;
+        margin-top: 4%;
+    }
+    i{
+        background-color:   rgba(255, 0, 0, 0.4);
+        border-radius: 4px;
+        padding: 0 2px 2px 2px;
+    }
+</style>
