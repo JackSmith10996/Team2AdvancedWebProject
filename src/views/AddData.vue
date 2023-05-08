1cc2a9e9c223f90df10118fe1731e55b017bbf39
JackSmith10996 <20052540@stu.mmu.ac.uk>
2023-05-08 12:35:34 +0100

    Sorted Add Data and deleted unused pages

--- src/views/AddData.vue
+++ src/views/AddData.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { ref } from "vue";
+import { ref} from "vue";
 import { useRouter } from "vue-router";
-import { doc , setDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import { doc , setDoc} from "firebase/firestore";
+import { firebaseFireStore } from "@/firebase/database";
 
 
-// data variables
 const ledv = ref("");
 const lesv = ref("");
 const lsv = ref("");
@@ -15,139 +15,135 @@ const resv = ref("");
 const rsv = ref("");
 const rvef = ref("");
 
-const data = {
-    ledv: ledv,
-    lesv: lesv,
-    lsv: lsv,
-    lvef: lvef,
-    lvmass: lvmass,
-    redv: redv,
-    resv: resv,
-    rsv: rsv,
-    rvef: rvef
-}
 
-//await setDoc(doc(db, "MyCardiomyopathy", /*User ID*/), data);
-
-//const docRef = doc(database, "MyCardiomyopathy", /*User ID*/)
-
-/*const updateData = await updateDoc(docRef, {
-    ledv: ledv,
-    lesv: lesv,
-    lsv: lsv,
-    lvef: lvef,
-    lvmass: lvmass,
-    redv: redv,
-    resv: resv,
-    rsv: rsv,
-    rvef: rvef
-});
-
-const updateTimestamp = await updateDoc(docRef, {
-    timestamp: serverTimestamp()
-});
-*/
 const router = useRouter();
 
+const addDataToFirestore = async () => {
+  try {
+    const data = {
+      ledv: parseFloat(ledv.value),
+      lesv: parseFloat(lesv.value),
+      lsv: parseFloat(lsv.value),
+      lvef: parseFloat(lvef.value),
+      lvmass: parseFloat(lvmass.value),
+      redv: parseFloat(redv.value),
+      resv: parseFloat(resv.value),
+      rsv: parseFloat(rsv.value),
+      rvef: parseFloat(rvef.value)
+    };
+
+    const randomId = Math.floor(Math.random() * 1000000);
+    const docRef = doc(firebaseFireStore,"MyCardiomyopathy",randomId.toString());
+    await setDoc(docRef, data);
+    console.log("Data added to Firestore successfully!");
+    console.log(data); 
+    console.log(randomId);
+    router.push("/");
+  } catch (error) {
+    console.error("Error adding data to Firestore: ", error);
+  }
+}
 </script>
+
 
 <template>
     <div class="addData-section">
-        <el-form label-width="95px" class="demo-ruleForm" @submit.prevent>
+      <el-form label-width="95px" class="demo-ruleForm" @submit.prevent>
         <h2 id="heading">Add Data</h2><br>
-
-            <el-form-item label="ledv">
-                <el-input
-                type="text"
-                placeholder="ledv"
-                required
-                autocomplete="off"
-                v-model="ledv"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="lesv">
-                <el-input
-                type="text"
-                placeholder="lesv"
-                required
-                autocomplete="off"
-                v-model="lesv"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="lsv">
-                <el-input
-                type="text"
-                placeholder="lsv"
-                required
-                autocomplete="off"
-                v-model="lsv"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="lvef">
-                <el-input
-                type="text"
-                placeholder="lvef"
-                required
-                autocomplete="off"
-                v-model="lvef"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="lvmass">
-                <el-input
-                type="text"
-                placeholder="lvmass"
-                required
-                autocomplete="off"
-                v-model="lvmass"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="redv">
-                <el-input
-                type="text"
-                placeholder="redv"
-                required
-                autocomplete="off"
-                v-model="redv"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="resv">
-                <el-input
-                type="text"
-                placeholder="resv"
-                required
-                autocomplete="off"
-                v-model="resv"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="rsv">
-                <el-input
-                type="text"
-                placeholder="rsv"
-                required
-                autocomplete="off"
-                v-model="rsv"
-                ></el-input>
-            </el-form-item>
-            <el-form-item label="rvef">
-                <el-input
-                type="text"
-                placeholder="rvef"
-                required
-                autocomplete="off"
-                v-model="rvef"
-                ></el-input>
-            </el-form-item>
-
-            <el-form-item>
-        <el-button type="danger" @click="updateData">Submit</el-button>
-        <router-link :to="{ name: 'Home' }"
-            ><el-button id="cancel-btn">Cancel</el-button></router-link
-        >
+        <el-form-item label="ledv">
+  <el-input
+    type="text"
+    placeholder="ledv"
+    required
+    autocomplete="off"
+    v-model="ledv"
+  ></el-input>
+</el-form-item>
+<el-form-item label="lesv">
+  <el-input
+    type="text"
+    placeholder="lesv"
+    required
+    autocomplete="off"
+    v-model="lesv"
+  ></el-input>
+</el-form-item>
+<el-form-item label="lsv">
+  <el-input
+    type="text"
+    placeholder="lsv"
+    required
+    autocomplete="off"
+    v-model="lsv"
+  ></el-input>
+</el-form-item>
+<el-form-item label="lvef">
+  <el-input
+    type="text"
+    placeholder="lvef"
+    required
+    autocomplete="off"
+    v-model="lvef"
+  ></el-input>
+</el-form-item>
+<el-form-item label="lvmass">
+  <el-input
+    type="text"
+    placeholder="lvmass"
+    required
+    autocomplete="off"
+    v-model="lvmass"
+  ></el-input>
+</el-form-item>
+<el-form-item label="redv">
+  <el-input
+    type="text"
+    placeholder="redv"
+    required
+    autocomplete="off"
+    v-model="redv"
+  ></el-input>
+</el-form-item>
+<el-form-item label="resv">
+  <el-input
+    type="text"
+    placeholder="resv"
+    required
+    autocomplete="off"
+    v-model="resv"
+  ></el-input>
+</el-form-item>
+<el-form-item label="rsv">
+  <el-input
+    type="text"
+    placeholder="rsv"
+    required
+    autocomplete="off"
+    v-model="rsv"
+  ></el-input>
+</el-form-item>
+<el-form-item label="rvef">
+  <el-input
+    type="text"
+    placeholder="rvef"
+    required
+    autocomplete="off"
+    v-model="rvef"
+  ></el-input>
+</el-form-item>
+        
+        
+  
+        <el-form-item>
+          <el-button type="danger" @click="addDataToFirestore">Submit</el-button>
+          <router-link :to="{ name: 'Home' }">
+            <el-button id="cancel-btn">Cancel</el-button>
+          </router-link>
         </el-form-item>
-            
-        </el-form>
+      </el-form>
     </div>
-</template>
+  </template>
+  
 
 <style>
 .addData-section{
